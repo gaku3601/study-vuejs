4ffabe9ad5a00299a7c83a7ef608f0ef871c5303
gaku0601 <pro.gaku@gmail.com>
2017-12-19 18:06:49 +0900

    test appの作成

---
+++ 08-normalizr/src/store/data.js
@@ -0,0 +1,25 @@
+export default{
+  namespaced: true,
+  state: {
+    counter: 1
+  },
+
+  mutations: {
+    updateCounter (state, payload) {
+      state.counter = payload
+    }
+  },
+  actions: {
+    fetchData ({ commit }) {
+      let data = [
+        {
+          'counter': 1,
+          'building': [
+            {'id': 1, 'name': 'test', tag: [{'id': 1, name: 'tag1'}]}
+          ]
+        }
+      ]
+      console.log(data[0].building[0].tag[0].name)
+    }
+  }
+}
---
+++ 08-normalizr/src/store/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import data from './data'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  strict: true,
+  modules: {
+    data
+  }
+})
